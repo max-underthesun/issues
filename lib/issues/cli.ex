a2f9b692ecd656f3bea20a7a2de51e179f0a8b5f
max_underthesun <max.underthesun@gmail.com>
2017-01-29 11:38:55 +0400

    small changes due to dependencies versions upgrade

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -7,7 +7,7 @@ defmodule Issues.CLI do
 
   @moduledoc """
   Handle the command line parsing and the dispatch to
-  the various functions that end up generating a 
+  the various functions that end up generating a
   table of the last _n_ issues in a github project
   """
 
@@ -16,8 +16,8 @@ defmodule Issues.CLI do
   @default_count 4
 
   def run(argv) do
-    argv 
-      |> parse_args 
+    argv
+      |> parse_args
       |> process
   end
 
